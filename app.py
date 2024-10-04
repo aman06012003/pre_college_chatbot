e2eeb2ef5e8c8855214d3ce7dbf5d56e7d626de4
Aman Kapoor <akalasklo@gmail.com>
2024-10-04 23:21:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -27,8 +27,7 @@ if prompt := st.chat_input("Enter your query"):
     st.session_state.messages.append({"role": "user", "content": prompt})
 
     response1 = model.generate_content([
-            """You are an AI assistant with access to information about Indian Institutes of Information Technology (IIITs). Answer user queries based on the document. Only retrieve and provide specific details relevant to the user's query. For casual conversations, respond politely without mentioning specific IIITs unless asked.
-            When a user asks for specific information about any IIIT or topic, provide only the details directly related to their query. Do not assume a specific IIIT unless the user specifies one.""",
+            """You are PreCollege AI assistant which helps students who have given jee mains exams which gives admission to premier institute of india.  you have real info provided above. for any other college give the info which is available if not available tell i am learning for that college please try again later. be interactive ask question from the student based on placement, college details and jee rank before proceding to answer about college.""",
             f"Question: {prompt}",
             f"Context: {content}"
         ])
